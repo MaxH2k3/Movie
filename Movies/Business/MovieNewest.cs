b78f4dabc5922e8f70ab6aae9c8f7d6400f1eb23
huy trần <huy110903@gmail.com>
2023-12-19 23:16:54 +0700

    update newest movie

--- Movies/Business/MovieNewest.cs
+++ Movies/Business/MovieNewest.cs
@@ -6,6 +6,7 @@
         public string? EnglishName { get; set; }
         public string? VietnamName { get; set; }
         public string? Description { get; set; }
+        public string? Thumbnail { get; set; }
         public string? Trailer { get; set; }
     }
 }
